793602aaef79d4d9bdb0640ac2d16aaae65d0e53
morimotohirotomo <minamoyori19@outlook.jp>
2020-04-18 00:50:04 +0900

    finish2_4~5

---
+++ Java_prac/src/practice2/Chap4.java
@@ -0,0 +1,13 @@
+package practice2;
+import java.util.Scanner;
+public class Chap4 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		System.out.println((x+y) + "," + (x-y) + "," + (x*y) + "," + (x/y) + "," + (x%y));
+	}
+
+}
