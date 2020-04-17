a33ae6bf1b8af9197ed14f158e5189b5251dc446
morimotohirotomo <minamoyori19@outlook.jp>
2020-04-18 01:13:06 +0900

    finish3_1

---
+++ Java_prac/src/practice3/Chap1.java
@@ -0,0 +1,19 @@
+package practice3;
+import java.util.Scanner;
+public class Chap1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		if (x > y) {
+			System.out.println(x);
+		} else {
+			System.out.println(y);
+		}
+
+		System.out.println((x > y)? x : y);
+	}
+
+}
