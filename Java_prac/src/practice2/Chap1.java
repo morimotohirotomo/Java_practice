72045d6643d10cfbcb2a8b3f1033cdd1a138fa1f
morimotohirotomo <minamoyori19@outlook.jp>
2020-05-15 20:12:32 +0900

    random

--- Java_prac/src/practice2/Chap1.java
+++ Java_prac/src/practice2/Chap1.java
@@ -7,6 +7,8 @@ public class Chap1 {
 		Scanner scanner = new Scanner(System.in);
 		String s = scanner.nextLine();
 		System.out.println(s);
+		int i = new java.util.Random().nextInt(10);
+		System.out.println(i);
 	}
 
 }
