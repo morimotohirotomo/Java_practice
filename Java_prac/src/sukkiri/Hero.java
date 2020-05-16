948c773acb4c1c63f31ef3be0b6a77dedbe05a7b
morimotohirotomo <minamoyori19@outlook.jp>
2020-05-16 17:28:13 +0900

    modify Hero and Matango

--- Java_prac/src/sukkiri/Hero.java
+++ Java_prac/src/sukkiri/Hero.java
@@ -10,6 +10,7 @@ public class Hero {
 	}
 
 	public void sit(int sec) {
+		this.hp += sec;
 		System.out.println(this.name + "は、" + sec + "秒座った！");
 		System.out.println("HPが" + sec + "ポイント回復した！");
 	}
