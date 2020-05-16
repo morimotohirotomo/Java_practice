d1218f10d513d7a45bc52e8a3b1bd86671bdb2de
morimotohirotomo <minamoyori19@outlook.jp>
2020-05-16 23:08:15 +0900

    add Hero costructor

--- Java_prac/src/sukkiri/Hero.java
+++ Java_prac/src/sukkiri/Hero.java
@@ -4,6 +4,15 @@ public class Hero {
 	String name;
 	int hp;
 
+	public Hero(String name) {
+		this.hp = 100;
+		this.name = name;
+	}
+
+	public Hero() {
+		this("ダミー");
+	}
+
 	public void sleep() {
 		this.hp = 100;
 		System.out.println(this.name + "は、眠って回復した！");
--- Java_prac/src/sukkiri/Main.java
+++ Java_prac/src/sukkiri/Main.java
@@ -2,21 +2,9 @@ package sukkiri;
 
 public class Main {
 	public static void main(String[] args) {
-		Hero h = new Hero();
-		h.name = "ミナト";
-		h.hp = 100;
-
-		Matango m1 = new Matango();
-		m1.hp = 50;
-		m1.suffix = 'A';
-
-		Matango m2 = new Matango();
-		m1.hp = 48;
-		m1.suffix = 'B';
-
-		h.slip();
-		m1.run();
-		m2.run();
-		h.run();
+		Hero h = new Hero("ミナト");
+		System.out.println(h.name + h.hp);
+		Hero h2 = new Hero();
+		System.out.println(h2.name + h2.hp);
 	}
 }
