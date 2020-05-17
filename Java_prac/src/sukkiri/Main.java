65cdb1b71f8bf4dc769d15acb2dc0cd1f5af9507
morimotohirotomo <minamoyori19@outlook.jp>
2020-05-17 19:05:14 +0900

    add PoisonMatango class

--- Java_prac/src/sukkiri/Main.java
+++ Java_prac/src/sukkiri/Main.java
@@ -2,7 +2,8 @@ package sukkiri;
 
 public class Main {
 	public static void main(String[] args) {
-		SuperHero sh = new SuperHero();
-		sh.run();
+		Hero h = new Hero("ミナト");
+		PoisonMatango pm = new PoisonMatango('A');
+		pm.attack(h);
 	}
 }
