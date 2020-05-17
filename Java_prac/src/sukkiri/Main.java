66cebec153c30e039e71b512049978b43bcfebdc
morimotohirotomo <minamoyori19@outlook.jp>
2020-05-17 12:20:13 +0900

    add SuperHero class

--- Java_prac/src/sukkiri/Main.java
+++ Java_prac/src/sukkiri/Main.java
@@ -2,9 +2,7 @@ package sukkiri;
 
 public class Main {
 	public static void main(String[] args) {
-		Hero h = new Hero("ミナト");
-		System.out.println(h.name + h.hp);
-		Hero h2 = new Hero();
-		System.out.println(h2.name + h2.hp);
+		SuperHero sh = new SuperHero();
+		sh.run();
 	}
 }
