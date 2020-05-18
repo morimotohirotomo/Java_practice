23cb430320e432fc6a3e65cf43e52007dccb4fff
morimotohirotomo <minamoyori19@outlook.jp>
2020-05-18 17:03:04 +0900

    add Time class

---
+++ Java_prac/src/sukkiri/Time.java
@@ -0,0 +1,10 @@
+package sukkiri;
+import java.util.Date;
+
+public class Time {
+	public static void main(String[] args) {
+		Date now = new Date();
+		System.out.println(now);
+		System.out.println(now.getTime());
+	}
+}
