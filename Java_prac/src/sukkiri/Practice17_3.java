393a62ad648cbfe5bf41c67c8bd2d80ace8f19d3
morimotohirotomo <minamoyori19@outlook.jp>
2020-05-20 11:16:18 +0900

    finish practice17_3

---
+++ Java_prac/src/sukkiri/Practice17_3.java
@@ -0,0 +1,14 @@
+package sukkiri;
+
+public class Practice17_3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		try {
+			int i = Integer.parseInt("三");
+		} catch (NumberFormatException e) {
+			System.out.println("NumberFormatException例外をcatchしました");
+		}
+	}
+
+}
