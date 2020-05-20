d4eeb4411f4b074d0cdac411a0287519fd40182b
morimotohirotomo <minamoyori19@outlook.jp>
2020-05-20 11:08:08 +0900

    finish practice17_1,2

--- Java_prac/src/sukkiri/Practice17_1.java
+++ Java_prac/src/sukkiri/Practice17_1.java
@@ -4,8 +4,14 @@ public class Practice17_1 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		String s = null;
-		System.out.println(s.length());
+		try {
+			String s = null;
+			System.out.println(s.length());
+		} catch (NullPointerException e) {
+			System.out.println("NullPointerException例外をcatchしました");
+			System.out.println("--スタックトレース--");
+			e.printStackTrace();
+		}
 	}
 
 }
