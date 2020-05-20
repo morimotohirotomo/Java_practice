f3121cf1d3de14da8ab05011c1fc1176a0c1d651
morimotohirotomo <minamoyori19@outlook.jp>
2020-05-20 11:03:25 +0900

    start practice17_1

---
+++ Java_prac/src/sukkiri/Practice17_1.java
@@ -0,0 +1,11 @@
+package sukkiri;
+
+public class Practice17_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String s = null;
+		System.out.println(s.length());
+	}
+
+}
