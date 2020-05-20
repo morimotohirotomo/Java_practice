3d734f0d0dbbeb4ac55718fef7faaf5e50197549
morimotohirotomo <minamoyori19@outlook.jp>
2020-05-20 11:19:36 +0900

    finish practice17_4

---
+++ Java_prac/src/sukkiri/Practice17_4.java
@@ -0,0 +1,11 @@
+package sukkiri;
+import java.io.IOException;
+
+public class Practice17_4 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO 自動生成されたメソッド・スタブ
+		throw new IOException();
+	}
+
+}
