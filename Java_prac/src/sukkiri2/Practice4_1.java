0f9029ce886e33929619cbf2120f95f3fbac4282
morimotohirotomo <minamoyori19@outlook.jp>
2020-05-21 21:29:31 +0900

    add equals()

--- Java_prac/src/sukkiri2/Practice4_1.java
+++ Java_prac/src/sukkiri2/Practice4_1.java
@@ -5,4 +5,14 @@ public class Practice4_1 {
 	private String title;
 	private Date publishDate;
 	private String comment;
+
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null) return false;
+		if (!(o instanceof Practice4_1)) return false;
+		Practice4_1 p = (Practice4_1) o;
+		if (!publishDate.equals(p.publishDate)) return false;
+		if (!title.equals(p.title)) return false;
+		return true;
+	}
 }
