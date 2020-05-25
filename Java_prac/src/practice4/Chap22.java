70670031386dbbc4c86e6c373428c39fce71c929
morimotohirotomo <minamoyori19@outlook.jp>
2020-05-25 21:44:00 +0900

    finish

--- Java_prac/src/practice4/Chap22.java
+++ Java_prac/src/practice4/Chap22.java
@@ -7,11 +7,23 @@ public class Chap22 {
 		int n1 = 0;
 		int n2 = 1;
 		int n3 = n1 + n2;
-		while( n1 <= 1000 ){
+		while( n1 <= 100){
 		    System.out.print( n1 + "," );
 		    n1 = n2;
 		    n2 = n3;
 		    n3 = n1 + n2;
+		}
+
+		for(int i = 0; i < 10; i++) {
+			System.out.println(fibo(i));
+		}
+	}
+
+	public static int fibo(int num) {
+		if (num < 2) {
+			return num;
+		} else {
+			return fibo(num - 1) + fibo(num - 2);
 		}
 	}
 
